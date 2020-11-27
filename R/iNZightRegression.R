88cabe69fa3fab15c519299de237d4ba26cb16aa
Tom Elliott <tom.elliott@auckland.ac.nz>
2020-11-27 09:40:02 +0000

    version 1.3.0

---
+++ R/iNZightRegression.R
@@ -0,0 +1,14 @@
+#' This package provides some additional functions to be used when
+#' fitting regression models. It supports linear, general linear, and
+#' survey (general) linear models.
+#'
+#' @title Additional functionality for iNZight
+#'
+#' @author Simon Potter, David Banks, Tom Elliott.
+#'
+#' @docType package
+#'
+#' @import stats graphics grDevices
+#'
+#' @name iNZightRegression
+NULL
